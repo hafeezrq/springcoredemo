904046021245bc541e6689d5b1db72b9cec107a6
hafeezrq <hafeezrq@yahoo.com>
2023-07-23 17:11:06 +1000

    Implement CricketCoach class and demo DI.

---
+++ src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class DemoController {
+  private Coach myCoach;
+
+  public DemoController(Coach theCoach) {
+    myCoach = theCoach;
+  }
+
+  @GetMapping(value = "/dailyworkouts")
+  public String getDailyWorkout() {
+    return myCoach.getDailyWorkout();
+  }
+
+}
