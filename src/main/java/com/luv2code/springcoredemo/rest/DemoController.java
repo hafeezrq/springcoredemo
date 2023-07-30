33da89abf3ab86e5f55bf1b4c43a6ed8bba307ac
hafeezrq <hafeezrq@yahoo.com>
2023-07-30 20:43:20 +1000

    Demo the setter injection.

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,13 +2,19 @@ package com.luv2code.springcoredemo.rest;
 
 import org.springframework.web.bind.annotation.RestController;
 import com.luv2code.util.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class DemoController {
   private Coach myCoach;
 
-  public DemoController(Coach theCoach) {
+  /*
+   * public DemoController(Coach theCoach) { myCoach = theCoach; }
+   */
+
+  @Autowired
+  public void setMyCoach(Coach theCoach) {
     myCoach = theCoach;
   }
 
