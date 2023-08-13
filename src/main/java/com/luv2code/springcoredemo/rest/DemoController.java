294cfaea16ffad2f09d4b6239388871973d5dd99
hafeezrq <hafeezrq@yahoo.com>
2023-08-13 20:39:26 +1000

    Demo the default Singlton Bean scope.

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,6 +1,5 @@
 package com.luv2code.springcoredemo.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +9,29 @@ import com.luv2code.util.Coach;
 @RestController
 public class DemoController {
   private Coach myCoach;
+  private Coach anotherCoach;
 
   /*
    * public DemoController(Coach theCoach) { myCoach = theCoach; }
    */
-  public DemoController() {
+  public DemoController(@Qualifier("cricketCoach") Coach theCoach, @Qualifier("cricketCoach") Coach theOtherCoch) {
+	    this.myCoach = theCoach;
+	    this.anotherCoach = theOtherCoch;
 }
 
-  @Autowired
-  public void setMyCoach(@Qualifier("cricketCoach") Coach theCoach) {
-    this.myCoach = theCoach;
-  }
+//  @Autowired
+//  public void setMyCoach(@Qualifier("cricketCoach") Coach theCoach) {
+//    this.myCoach = theCoach;
+//  }
 
   @GetMapping(value = "/dailyworkouts")
   public String getDailyWorkout() {
     return this.myCoach.getDailyWorkout();
+  }
+  
+  @GetMapping(value="/check")
+  public String compareBeans() {
+	  return "myCoach is equal to anotherCoach " + (myCoach == anotherCoach);
   }
 
 }
